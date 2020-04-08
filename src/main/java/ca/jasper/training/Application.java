e1a3a9446dcef9f8a2f8389e470d862daddddf42
Jasper Zhao <jasper_zs@hotmail.com>
2020-04-07 23:42:56 -0400

    Configure compiler source and target version

--- src/main/java/ca/jasper/training/Application.java
+++ src/main/java/ca/jasper/training/Application.java
@@ -1,6 +1,18 @@
 package ca.jasper.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    private void greet() {
+        final List<String> greetings = new ArrayList<>();
+        greetings.add("Doris");
+        greetings.add("Jasper");
+
+        for (final String greeting : greetings) {
+           System.out.println(greeting);
+        }
+    }
 
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +22,6 @@ public class Application {
     public static final void main (final String[] args) {
         System.out.println ("Starting Application");
         Application app = new Application();
+        app.greet();
     }
 }
